2faf3578b5dffba0b0d1af1240ad012fd541d20d
Gary Capell <ook@google.com>
2012-08-21 16:07:27 +1000

    gofmt

--- hello/hello.go
+++ hello/hello.go
@@ -1,36 +1,36 @@
 package hello
 
 import (
-    "appengine"
-    "appengine/datastore"
-    "appengine/user"
-    "html/template"
-    "net/http"
-    "time"
+	"appengine"
+	"appengine/datastore"
+	"appengine/user"
+	"html/template"
+	"net/http"
+	"time"
 )
 
 type Greeting struct {
-    Author  string
-    Content string
-    Date    time.Time
+	Author  string
+	Content string
+	Date    time.Time
 }
 
 func init() {
-    http.HandleFunc("/", root)
-    http.HandleFunc("/sign", sign)
+	http.HandleFunc("/", root)
+	http.HandleFunc("/sign", sign)
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
-    q := datastore.NewQuery("Greeting").Order("-Date").Limit(10)
-    greetings := make([]Greeting, 0, 10)
-    if _, err := q.GetAll(c, &greetings); err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    if err := guestbookTemplate.Execute(w, greetings); err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
+	c := appengine.NewContext(r)
+	q := datastore.NewQuery("Greeting").Order("-Date").Limit(10)
+	greetings := make([]Greeting, 0, 10)
+	if _, err := q.GetAll(c, &greetings); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := guestbookTemplate.Execute(w, greetings); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 var guestbookTemplate = template.Must(template.New("book").Parse(guestbookTemplateHTML))
@@ -55,18 +55,18 @@ const guestbookTemplateHTML = `
 `
 
 func sign(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
-    g := Greeting{
-        Content: r.FormValue("content"),
-        Date:    time.Now(),
-    }
-    if u := user.Current(c); u != nil {
-        g.Author = u.String()
-    }
-    _, err := datastore.Put(c, datastore.NewIncompleteKey(c, "Greeting", nil), &g)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    http.Redirect(w, r, "/", http.StatusFound)
+	c := appengine.NewContext(r)
+	g := Greeting{
+		Content: r.FormValue("content"),
+		Date:    time.Now(),
+	}
+	if u := user.Current(c); u != nil {
+		g.Author = u.String()
+	}
+	_, err := datastore.Put(c, datastore.NewIncompleteKey(c, "Greeting", nil), &g)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/", http.StatusFound)
 }
