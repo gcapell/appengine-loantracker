66f3b831f5e81da73d5f36e39d913f663afdc32c
Gary Capell <gary.capell@gmail.com>
2012-08-22 19:55:41 +1000

    Parse entered amounts

--- hello/hello.go
+++ hello/hello.go
@@ -9,7 +9,10 @@ import (
 	"time"
 	"log"
 	"fmt"
+	"regexp"
+	"strconv"
 )
+
 
 type Cents int
 
@@ -30,6 +33,30 @@ func (c Cents) String() string {
 	return fmt.Sprintf("$%d.%02d", dollars, cents)
 }
 
+var amountRE = regexp.MustCompile(`(\d+)(.\d\d)?`)
+
+func ParseCents(s string) (Cents, error) {
+	matches := amountRE.FindStringSubmatch(s)
+	log.Println("matches", matches)
+	if matches == nil || len(matches)<2 {
+		return 0, fmt.Errorf("could not parse %q into dollar amount", s)
+	}
+	dollars, err := strconv.Atoi(matches[1])
+	if err != nil {
+		return 0, fmt.Errorf("Bad dollar amount:%q", matches[1])
+	}
+	cents := 0
+	if len(matches)==3 {
+		cents, err = strconv.Atoi(matches[2][1:])
+		if err != nil {
+			return 0, fmt.Errorf("ParseCents: Bad cents amount:%q in %q", matches[2], s)
+		}
+	}
+
+	return Cents(dollars * 100 + cents), nil
+}
+
+
 var paymentTemplate = template.Must(template.New("book").Parse(paymentTemplateHTML))
 
 const paymentTemplateHTML = `
@@ -39,7 +66,7 @@ const paymentTemplateHTML = `
     {{range .}}
 	<tr>
 	<td>{{.Amount}}</td>
-	<td>{{.Date.Format "2 Jan 2006"}}</td>
+	<td>{{.Date.Format "15:04 2 Jan 2006"}}</td>
 	</tr>
 	{{end}}
     <form action="/addPayment" method="post">
@@ -91,14 +118,17 @@ func root(w http.ResponseWriter, r *http.Request) error {
 
 func addPayment(w http.ResponseWriter, r *http.Request) error {
 	c := appengine.NewContext(r)
-	amount := r.FormValue("amount")
+	amount, err := ParseCents(r.FormValue("amount"))
+	if err != nil {
+		return err
+	}
 	dateString := r.FormValue("date")
 	log.Println("amount", amount, "date", dateString)
 	userName := ""
 	if u := user.Current(c); u != nil {
 		userName = u.String()
 	}
-	p := Payment{time.Now(), userName, 207}
+	p := Payment{time.Now(), userName, amount}
 	if _, err := datastore.Put(c, datastore.NewIncompleteKey(c, "Payment", nil), &p); err != nil {
 		return err
 	}
